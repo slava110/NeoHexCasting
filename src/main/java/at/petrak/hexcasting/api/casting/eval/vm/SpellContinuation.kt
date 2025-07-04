5bd89bcfbfedb50287012cf80aba7bf90f180927
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-07-04 15:13:51 -0700

    Fix SpellContinuation serialization
    Co-authored-by: Aly Cerruti <me@s5.pm>

--- src/main/java/at/petrak/hexcasting/api/casting/eval/vm/SpellContinuation.kt
+++ src/main/java/at/petrak/hexcasting/api/casting/eval/vm/SpellContinuation.kt
@@ -1,13 +1,6 @@
 package at.petrak.hexcasting.api.casting.eval.vm
 
-import com.mojang.datafixers.util.Either
-import com.mojang.serialization.Codec
-import com.mojang.serialization.codecs.RecordCodecBuilder
-import io.netty.buffer.ByteBuf
-import net.minecraft.network.RegistryFriendlyByteBuf
 import net.minecraft.network.codec.ByteBufCodecs
-import net.minecraft.network.codec.StreamCodec
-import java.util.function.Function
 
 /**
  * A continuation during the execution of a spell.
@@ -20,42 +13,38 @@ sealed interface SpellContinuation {
     fun pushFrame(frame: ContinuationFrame): SpellContinuation = NotDone(frame, this)
 
     companion object {
-        // TODO port: maybe serialize to list like before?
-        // TODO port: maybe unit should be first
-        @JvmStatic
-        val CODEC = Codec.recursive<SpellContinuation>(
-            SpellContinuation::class.java.simpleName
-        ) { recursed: Codec<SpellContinuation> ->
-            Codec.withAlternative<SpellContinuation>(
-                Codec.unit(Done),
-                RecordCodecBuilder.create<NotDone> { inst ->
-                    inst.group(
-                        ContinuationFrame.Type.TYPED_CODEC.fieldOf("frame").forGetter { it.frame },
-                        recursed.fieldOf("next").forGetter { it.next }
-                    ).apply(inst, ::NotDone)
-                }
-            )
-        }
-        @JvmStatic
-        val STREAM_CODEC = StreamCodec.recursive<RegistryFriendlyByteBuf, SpellContinuation> { recursed ->
-            withAlternative(
-                StreamCodec.unit(Done),
-                StreamCodec.composite(
-                    ContinuationFrame.Type.TYPED_STREAM_CODEC, NotDone::frame,
-                    recursed, NotDone::next,
-                    ::NotDone
-                ).map(Function.identity()) { it as NotDone }
-            )
+        fun fromList(frames: List<ContinuationFrame>): SpellContinuation {
+            var accumulator: SpellContinuation = Done
+            val it = frames.listIterator(frames.size)
+            while (it.hasPrevious()) {
+                accumulator = NotDone(it.previous(), accumulator)
+            }
+            return accumulator
         }
 
-        private fun <B: ByteBuf, T> withAlternative(primary: StreamCodec<B, T>, alternative: StreamCodec<B, T>): StreamCodec<B, T> {
-            return ByteBufCodecs.either<B, T, T>(
-                primary,
-                alternative
-            ).map<T>(
-                Function { either: Either<T, T> -> Either.unwrap(either) },
-                Function { value: T -> Either.left(value) }
-            )
+        fun toList(continuation: SpellContinuation): List<ContinuationFrame> {
+            val accumulator = ArrayList<ContinuationFrame>()
+            var c = continuation
+            while (c != Done) {
+                accumulator.add((c as NotDone).frame)
+                c = c.next
+            }
+            return accumulator
         }
+
+        // TODO port: maybe unit should be first
+        @JvmStatic
+        val CODEC = ContinuationFrame.Type.TYPED_CODEC.listOf().xmap<SpellContinuation>(
+            SpellContinuation::fromList,
+            SpellContinuation::toList
+        )
+
+        @JvmStatic
+        val STREAM_CODEC = ContinuationFrame.Type.TYPED_STREAM_CODEC.apply(
+            ByteBufCodecs.list()
+        ).map(
+            SpellContinuation::fromList,
+            SpellContinuation::toList
+        )
     }
 }
